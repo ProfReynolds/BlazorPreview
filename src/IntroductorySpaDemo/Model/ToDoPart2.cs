3ea08a7ee8647cd660d97b69d02e501eca4087bd
Mark Reynolds <Mark@ProfReynolds.com>
2019-09-17 11:54:05 -0500

    Blazor demonstrator as presented at the Houston Techfest September 14, 2019

---
+++ src/IntroductorySpaDemo/Model/ToDoPart2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroductorySpaDemo.Model
+{
+    public class ToDoPart2
+    {
+        public string Text { get; set; }
+        public bool Done { get; set; }
+    }
+}
