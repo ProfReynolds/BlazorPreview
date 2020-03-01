a3bf37834c7c01148a4d113f709441d7cdf472ce
Mark Reynolds <Mark@ProfReynolds.com>
2020-03-01 07:58:35 -0600

    Review and redeploy blazor demo

---
+++ src/BlazorWebAssemblyDemo/MaintainCurrentCount.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorWebAssemblyDemo
+{
+    class MaintainCurrentCount
+    {
+        private static int _currentCount = 0;
+
+        public static int GetCurrentCount()
+        {
+            return _currentCount;
+        }
+
+        public static void IncrementCurrentCount()
+        {
+            _currentCount++;
+        }
+    }
+
+}
---
+++ src/IntroductorySpaDemo/MaintainCurrentCount.cs
@@ -0,0 +1,18 @@
+﻿namespace IntroductorySpaDemo
+{
+    class MaintainCurrentCount
+    {
+        private static int _currentCount = 0;
+
+        public static int GetCurrentCount()
+        {
+            return _currentCount;
+        }
+
+        public static void IncrementCurrentCount()
+        {
+            _currentCount++;
+        }
+    }
+
+}
